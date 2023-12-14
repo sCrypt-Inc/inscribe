acb1b1fc670630d5f2a1192a3d45975e7d22f8b4
aaron67 <aaron67@aaron67.cc>
2023-12-15 04:26:17 +0800

    Fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -208,12 +208,12 @@ function Home() {
         )}
         {connected() && _tabIndex === 2 && (
           <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-            <BSV20v1 _ordiAddress={_ordiAddress} _signer={_signer.current} />
+            <BSV20v1 _ordiAddress={_ordiAddress} _signer={_signer.current} _network={_network} />
           </Box>
         )}
         {connected() && _tabIndex === 3 && (
           <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-            <BSV20v2 _ordiAddress={_ordiAddress} _signer={_signer.current} _network={_network} />
+            <BSV20v2 _ordiAddress={_ordiAddress} _signer={_signer.current} />
           </Box>
         )}
       </Box>
